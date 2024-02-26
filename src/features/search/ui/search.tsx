a995dc656fe716d2250f014845bf0a694ec2b448
Dania <danila.lusuy@gmail.com>
2024-02-26 21:26:14 +0300

    feat: created basic Search component

--- src/features/search/ui/search.tsx
+++ src/features/search/ui/search.tsx
@@ -1,3 +1,18 @@
+"use client";
+import { ChangeEvent, useState } from "react";
+import { Input } from "@/shared/ui";
+
 export const Search = () => {
-  return <section>SEARCH</section>;
+  const [searchValue, setSearchValue] = useState("");
+  const handleChange = (e: ChangeEvent<HTMLInputElement>) => {
+    setSearchValue(e.target.value);
+  };
+  return (
+    <Input
+      className="text-slate-950 rounded-xl"
+      value={searchValue}
+      onChange={handleChange}
+      placeholder="search..."
+    />
+  );
 };
