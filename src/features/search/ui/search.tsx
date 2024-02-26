a015c95b23bb6a58f52f7b054eb0c007f80b4a59
Dania <danila.lusuy@gmail.com>
2024-02-26 23:14:41 +0300

    feat: added basic searchBar

--- src/features/search/ui/search.tsx
+++ src/features/search/ui/search.tsx
@@ -1,15 +1,18 @@
 "use client";
 import { ChangeEvent, useState } from "react";
 import { Input } from "@/shared/ui";
-
+import { useSearchStore } from "..";
+// Todo возможно можно обойтись без стора, и просто прокидывать value и onChange пропсами, так будет лучше
 export const Search = () => {
-  const [searchValue, setSearchValue] = useState("");
+  // const [searchValue, setSearchValue] = useState("");
+  const { searchValue, setSearchValue } = useSearchStore();
   const handleChange = (e: ChangeEvent<HTMLInputElement>) => {
     setSearchValue(e.target.value);
   };
+
   return (
     <Input
-      className="text-slate-950 rounded-xl"
+      className="text-slate-950 rounded-xl text-xl"
       value={searchValue}
       onChange={handleChange}
       placeholder="search..."
