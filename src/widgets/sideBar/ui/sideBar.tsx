9f532f657f5f3d750d9e85fba6b564ebe5e73024
Dania <danila.lusuy@gmail.com>
2024-02-29 18:30:51 +0300

    feat: changed sideBar logic, server component

--- src/widgets/sideBar/ui/sideBar.tsx
+++ src/widgets/sideBar/ui/sideBar.tsx
@@ -1,4 +1,3 @@
-"use client";
 import { FC, useEffect, useState } from "react";
 import { CategoriesWithLang, getAvailable } from "@/entities/categories";
 import { Exchanger, getExchangers } from "@/entities/exchanger";
@@ -10,35 +9,15 @@ import styles from "./sideBar.module.scss";
 
 interface SideBarProps {}
 
-export const SideBar: FC<SideBarProps> = (props) => {
-  // имеет смысл попробовать доставать данные из кеша а не создавать useState
-  const [availableValutes, setAvailableValutes] = useState<CategoriesWithLang>();
-  const [availableDestinations, setAvailableDestinations] = useState<CategoriesWithLang>();
-
-  const to = useSelectsStore((state) => state.giveSelect?.code_name);
-  const from = useSelectsStore((state) => state.getSelect?.code_name);
-
-  const state = useSelectsStore((state) => state);
-  useEffect(() => {
-    getAvailable("all").then((availableValutes) => setAvailableValutes(availableValutes));
-  }, []);
-  useEffect(() => {
-    if (to)
-      getAvailable(to).then((availableDestinations) => {
-        setAvailableDestinations(availableDestinations);
-      });
-  }, [to]);
-
-  /// добавить сюда запрос getAvailable и передать дату в ValutesList
-  /// как то сохранять в этом стейте выбранные валюты + передавать их в селектКард
-  /// добавить useEffect для рефетча и + учет get give как то
+export const SideBar = async (props: SideBarProps) => {
+  const availableValutes = await getAvailable("all");
 
   return (
     <section className={styles.sidebar}>
       <SelectCard type={selectType.give} />
       <ValutesList selectType={selectType.give} categories={availableValutes} />
       <SelectCard type={selectType.get} />
-      <ValutesList selectType={selectType.get} categories={availableDestinations} />
+      <ValutesList selectType={selectType.get} />
     </section>
   );
 };
