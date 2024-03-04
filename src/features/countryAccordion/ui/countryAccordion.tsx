048e083888e48ae1a80f88daf3ef3996f8675618
Dania <danila.lusuy@gmail.com>
2024-03-04 21:49:43 +0300

    feat:added CountryAccordion component and filtering logic

---
+++ src/features/countryAccordion/ui/countryAccordion.tsx
@@ -0,0 +1,48 @@
+"use client";
+
+import { memo, useMemo } from "react";
+import { Country, useCountryStore } from "@/entities/country";
+import { Accordion, AccordionContent, AccordionItem, AccordionTrigger } from "@/shared/ui";
+type CountryAccordionProps = {
+  countries: Country[];
+  show?: boolean;
+  searchCountry: string;
+};
+export const CountryAccordion = memo((props: CountryAccordionProps) => {
+  const { countries, show, searchCountry } = props;
+  const setCountry = useCountryStore((state) => state.setCountry);
+
+  const filteredCountries = useMemo(() => {
+    return countries.filter((country) => {
+      const countryName = country?.name?.ru.toLowerCase();
+      const filteredCities = country.cities.filter((city) =>
+        city?.name?.ru.toLowerCase().includes(searchCountry.toLowerCase()),
+      );
+      return countryName.includes(searchCountry.toLowerCase()) || filteredCities.length;
+    });
+  }, [countries, searchCountry]);
+
+  return (
+    <>
+      {show ? (
+        <Accordion className="w-full" collapsible type="single">
+          {filteredCountries?.map((country) => (
+            <AccordionItem className="text-nowrap" value={country?.name.ru} key={country?.id}>
+              <AccordionTrigger>{country?.name.ru}</AccordionTrigger>
+              <AccordionContent
+                className="cursor-pointer text-base"
+                onClick={() => setCountry(country)}
+              >
+                {country?.cities.map((city) => (
+                  <div className="p-2 hover:bg-neutral-600 rounded" key={city.id}>
+                    {city?.name?.ru}
+                  </div>
+                ))}
+              </AccordionContent>
+            </AccordionItem>
+          ))}
+        </Accordion>
+      ) : null}
+    </>
+  );
+});
