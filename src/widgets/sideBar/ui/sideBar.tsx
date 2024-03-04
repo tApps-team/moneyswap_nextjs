bedacad856b6394a7aee7090cf8dd9be9e28e5a7
Dania <danila.lusuy@gmail.com>
2024-03-04 21:52:44 +0300

    feat: added countries fetch and CountriesList component

--- src/widgets/sideBar/ui/sideBar.tsx
+++ src/widgets/sideBar/ui/sideBar.tsx
@@ -1,9 +1,12 @@
 import { FC, useEffect, useState } from "react";
+import { CountryAccordion } from "@/features/countryAccordion";
 import { CategoriesWithLang, getAvailable } from "@/entities/categories";
+import { getCountries } from "@/entities/country";
 import { Exchanger, getExchangers } from "@/entities/exchanger";
 import { SelectCard } from "@/entities/select";
 import { useSelectsStore } from "@/entities/valute";
 import { selectType } from "@/shared/types";
+import { CountriesList } from "../countriesList";
 import { ValutesList } from "../valutesList";
 import styles from "./sideBar.module.scss";
 
@@ -11,9 +14,10 @@ interface SideBarProps {}
 
 export const SideBar = async (props: SideBarProps) => {
   const availableValutes = await getAvailable("all");
-
+  const countries = await getCountries();
   return (
     <section className={styles.sidebar}>
+      <CountriesList countries={countries} />
       <SelectCard type={selectType.give} />
       <ValutesList selectType={selectType.give} categories={availableValutes} />
       <SelectCard type={selectType.get} />
