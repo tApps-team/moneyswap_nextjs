e265ab9e6cb2c8ec5d31710dc2fc782e4f2d1253
SenezAr <danila.lusuy@gmail.com>
2024-03-05 20:32:34 +0300

    feat: wrapped searchCountry in useThrottle

--- src/widgets/sideBar/countriesList/ui/countriesList.tsx
+++ src/widgets/sideBar/countriesList/ui/countriesList.tsx
@@ -3,21 +3,29 @@ import { useCallback, useState } from "react";
 import { CountryAccordion } from "@/features/countryAccordion";
 import { Search } from "@/features/search";
 import { Country, getCountries } from "@/entities/country";
+import { useThrottle } from "@/shared/lib";
 type CountriesListProps = {
   countries: Country[];
+  show?: boolean;
 };
 export const CountriesList = (props: CountriesListProps) => {
-  const { countries } = props;
+  const { countries, show } = props;
   const [searchCountry, setSearchCountry] = useState("");
+  //Todo Обсудить нужен ли throttle или нет
+  const searchCountryThrottled = useThrottle(searchCountry, 500);
 
   const onChange = useCallback((searchValue: string) => {
     setSearchCountry(searchValue);
   }, []);
 
   return (
-    <div>
-      <Search onChange={onChange} searchValue={searchCountry} />
-      <CountryAccordion show searchCountry={searchCountry} countries={countries} />
-    </div>
+    <>
+      {show ? (
+        <div>
+          <Search onChange={onChange} searchValue={searchCountry} />
+          <CountryAccordion show searchCountry={searchCountryThrottled} countries={countries} />
+        </div>
+      ) : null}
+    </>
   );
 };
