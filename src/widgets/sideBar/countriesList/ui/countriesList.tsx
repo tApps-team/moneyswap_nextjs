e3428035f5282a8b582d613fb10ae82dd4e665b7
Dania <danila.lusuy@gmail.com>
2024-03-06 17:00:28 +0300

    feat: added useDebounce for searchCounrty value

--- src/widgets/sideBar/countriesList/ui/countriesList.tsx
+++ src/widgets/sideBar/countriesList/ui/countriesList.tsx
@@ -1,9 +1,9 @@
 "use client";
-import { useCallback, useState } from "react";
+import { useCallback, useDeferredValue, useState } from "react";
 import { CountryAccordion } from "@/features/countryAccordion";
 import { Search } from "@/features/search";
 import { Country, getCountries } from "@/entities/country";
-import { useThrottle } from "@/shared/lib";
+import { useDebounce, useThrottle } from "@/shared/lib";
 type CountriesListProps = {
   countries: Country[];
   show?: boolean;
@@ -12,7 +12,7 @@ export const CountriesList = (props: CountriesListProps) => {
   const { countries, show } = props;
   const [searchCountry, setSearchCountry] = useState("");
   //Todo Обсудить нужен ли throttle или нет
-  const searchCountryThrottled = useThrottle(searchCountry, 500);
+  const searchCountryThrottled = useDebounce(searchCountry, 300);
 
   const onChange = useCallback((searchValue: string) => {
     setSearchCountry(searchValue);
