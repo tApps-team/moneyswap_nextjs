624c22a077aa28c6155b1613dac44efa0f3691fb
SenezAr <danila.lusuy@gmail.com>
2024-03-10 14:35:30 +0300

    feat: added country modal component

--- src/features/countryModal/ui/countryModal.tsx
+++ src/features/countryModal/ui/countryModal.tsx
@@ -1,7 +1,8 @@
 "use client";
 
 import Image from "next/image";
-import { useCountryStore } from "@/entities/country";
+import { useCallback, useEffect, useMemo, useState } from "react";
+import { CityCard, Country, CountryCard, getCountries, useCountryStore } from "@/entities/country";
 import {
   Button,
   Dialog,
@@ -16,6 +17,36 @@ import {
 
 export const CountryModal = () => {
   const country = useCountryStore((state) => state.country);
+  const [countries, setCountries] = useState<Country[]>();
+  const [searchCountry, setSearchCountry] = useState("");
+  const [currentCountryId, setCurrentCountryId] = useState<number | null>(null);
+  const handleChange = useCallback((value: string) => {
+    setSearchCountry(value);
+    setCurrentCountryId(null);
+  }, []);
+  const handleClick = useCallback((currentCountry: number) => {
+    setCurrentCountryId(currentCountry);
+  }, []);
+  useEffect(() => {
+    getCountries().then((countries) => setCountries(countries));
+  }, []);
+  console.log(currentCountryId);
+  const filteredCountries = useMemo(() => {
+    return countries?.filter((country) => {
+      const countryName = country?.name?.ru.toLowerCase();
+      const filteredCities = country.cities.filter((city) =>
+        city?.name?.ru.toLowerCase().includes(searchCountry.toLowerCase()),
+      );
+      return countryName.includes(searchCountry.toLowerCase()) || filteredCities.length;
+    });
+  }, [countries, searchCountry]);
+
+  const filteredCities = useMemo(() => {
+    const selectedCountry = countries?.find((country) => country.id === currentCountryId);
+
+    return selectedCountry?.cities;
+  }, [countries, currentCountryId]);
+
   return (
     <Dialog>
       <DialogTrigger asChild>
@@ -33,17 +64,30 @@ export const CountryModal = () => {
           <Button>Выберите город </Button>
         )}
       </DialogTrigger>
-      <DialogContent className="max-w-[425px]">
+      <DialogContent className="min-w-[800px]">
         <DialogHeader>
           <DialogTitle>Выбрать город</DialogTitle>
           <DialogDescription>
-            <Input placeholder="Поиск города и страны" />
+            <Input
+              value={searchCountry}
+              onChange={(e) => handleChange(e.target.value)}
+              className="focus-visible:ring-transparent"
+              placeholder="Поиск города и страны"
+            />
           </DialogDescription>
         </DialogHeader>
+        <div className="flex">
+          <div className="border w-96 max-h-[300px] rounded-lg overflow-y-scroll ">
+            {filteredCountries?.map((country) => (
+              <CountryCard onClick={handleClick} key={country.id} country={country} />
+            ))}
+          </div>
+          <div className="border w-96 max-h-[300px] rounded-lg  overflow-y-scroll">
+            {filteredCities?.map((city) => <CityCard key={city.id} city={city} />)}
+          </div>
+        </div>
 
-        <DialogFooter>
-          <Button type="submit">Save changes</Button>
-        </DialogFooter>
+        <DialogFooter></DialogFooter>
       </DialogContent>
     </Dialog>
   );
