61d8aa4ffbfae09d24d85f55274314e8b5fcf04a
Dania <danila.lusuy@gmail.com>
2024-03-11 23:39:00 +0300

    feat: added city  and setCity action

--- src/entities/country/model/store/countryStore.ts
+++ src/entities/country/model/store/countryStore.ts
@@ -1,14 +1,18 @@
 import { create } from "zustand";
 
 import { persist } from "zustand/middleware";
-import { Country } from "../..";
+import { City, Country } from "../..";
 
 type CountryStore = {
   country: Country | null;
+  city: City | null;
+  setCity: (city: City) => void;
   setCountry: (country: Country) => void;
 };
 
 export const useCountryStore = create<CountryStore>()((set) => ({
+  city: null,
   country: null,
+  setCity: (city) => set({ city: city }),
   setCountry: (country) => set({ country: country }),
 }));
