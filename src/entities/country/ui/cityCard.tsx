779dd86d5a6b72589c7449bb55d4812ab8530956
Dania <danila.lusuy@gmail.com>
2024-03-12 20:47:39 +0300

    feat(refactor later!): added DialogClose

--- src/entities/country/ui/cityCard.tsx
+++ src/entities/country/ui/cityCard.tsx
@@ -1,7 +1,7 @@
 /* eslint-disable jsx-a11y/no-static-element-interactions */
 /* eslint-disable jsx-a11y/click-events-have-key-events */
 import Image from "next/image";
-import { Card, CardContent } from "@/shared/ui";
+import { Card, CardContent, DialogClose } from "@/shared/ui";
 import { City } from "..";
 type CityCardProps = {
   city: City;
@@ -12,9 +12,11 @@ export const CityCard = (props: CityCardProps) => {
   return (
     <Card className="border-none rounded-none">
       <CardContent className="py-2 hover:bg-slate-400">
-        <div onClick={() => onClick(city)} className="flex items-center cursor-pointer ">
-          <div>{city?.name.ru}</div>
-        </div>
+        <DialogClose asChild>
+          <div onClick={() => onClick(city)} className="flex items-center cursor-pointer ">
+            <div>{city?.name.ru}</div>
+          </div>
+        </DialogClose>
       </CardContent>
     </Card>
   );
