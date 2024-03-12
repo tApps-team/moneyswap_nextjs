72a38bbb49429f6a0a8c7f8fad94bc72837bd83a
Dania <danila.lusuy@gmail.com>
2024-03-12 20:50:37 +0300

    feat: added select name

--- src/widgets/sideBar/valutesList/ui/valutesList.tsx
+++ src/widgets/sideBar/valutesList/ui/valutesList.tsx
@@ -1,11 +1,11 @@
 "use client";
-import { ChangeEvent, FC, memo, useCallback, useEffect, useMemo, useState } from "react";
+
+import { FC, memo, useCallback, useEffect, useMemo, useState } from "react";
 import { DirectionTabs } from "@/features/directionTabs";
-import { Search, useSearchStore } from "@/features/search";
 import { CategoriesWithLang, getAvailable } from "@/entities/categories";
 import { ValuteCard, useSelectsStore } from "@/entities/valute";
 import { selectType } from "@/shared/types";
-import { Input } from "@/shared/ui";
+import { Button, Dialog, DialogClose, DialogContent, DialogTrigger, Input } from "@/shared/ui";
 import styles from "./valutesList.module.scss";
 
 interface ValutesListProps {
@@ -17,10 +17,15 @@ export const ValutesList: FC<ValutesListProps> = memo((props) => {
   const { selectType, categories } = props;
 
   const [searchValute, setSearchValute] = useState("");
+
   const [directionExchange, setDirectionExchange] = useState("");
   const [availableDestinations, setAvailableDestinations] = useState<CategoriesWithLang>();
 
   const toExchange = useSelectsStore((state) => state.giveSelect?.code_name);
+
+  const getSelectName = useSelectsStore((state) => state.getSelect?.name);
+  const giveSelectName = useSelectsStore((state) => state.giveSelect?.name);
+  const currentSelectName = selectType === "give" ? giveSelectName : getSelectName;
 
   const currentCategories = selectType === "give" ? categories : availableDestinations;
 
@@ -47,19 +52,29 @@ export const ValutesList: FC<ValutesListProps> = memo((props) => {
 
   return (
     <section className={styles.list}>
-      <DirectionTabs
-        directionExchange={directionExchange}
-        setDirectionExchange={onChangeDirectionExchager}
-        directions={currentCategories?.ru}
-      />
-      <Input
-        className="text-slate-950 rounded-xl text-xl"
-        onChange={(e) => onChange(e.target.value.trim())}
-        value={searchValute}
-      />
-      {filteredValutes?.map((valute) => (
-        <ValuteCard key={valute.id} valute={valute} type={selectType} />
-      ))}
+      <Dialog>
+        <DialogTrigger asChild>
+          <Button>Отдаю {currentSelectName} </Button>
+        </DialogTrigger>
+        <DialogContent className="min-w-[600px] bg-current">
+          <div className="min-h-[400px] grid grid-cols-1 grid-rows-2">
+            <DirectionTabs
+              directionExchange={directionExchange}
+              setDirectionExchange={onChangeDirectionExchager}
+              directions={currentCategories?.ru}
+            />
+            <Input
+              className="text-slate-950 rounded-xl text-xl"
+              onChange={(e) => onChange(e.target.value.trim())}
+              value={searchValute}
+            />
+
+            {filteredValutes?.map((valute) => (
+              <ValuteCard key={valute.id} valute={valute} type={selectType} />
+            ))}
+          </div>
+        </DialogContent>
+      </Dialog>
     </section>
   );
 });
