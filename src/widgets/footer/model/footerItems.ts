aefb5ff263b2730210838a02c574f461fe17cddd
Dany <danila.lusuy@gmail.com>
2024-07-17 18:29:36 +0300

    feat: add footerItems

---
+++ src/widgets/footer/model/footerItems.ts
@@ -0,0 +1,30 @@
+type FooterItems = {
+  href: string;
+  value: string;
+};
+export const footerItems: FooterItems[] = [
+  {
+    href: "/",
+    value: "lorem",
+  },
+  {
+    href: "/",
+    value: "lorem",
+  },
+  {
+    href: "/",
+    value: "lorem",
+  },
+  {
+    href: "/",
+    value: "lorem",
+  },
+  {
+    href: "/",
+    value: "lorem",
+  },
+  {
+    href: "/",
+    value: "lorem",
+  },
+];
