b4e5912baacdfbf5efd139afaec12fbd48a60c51
Dany <danila.lusuy@gmail.com>
2024-07-17 18:29:20 +0300

    fix: remove text and tags

--- src/views/main/ui/main.tsx
+++ src/views/main/ui/main.tsx
@@ -1,17 +1,7 @@
 export const Main = async () => {
   return (
-    <>
-      {/* <SideBar /> */}
-      <section className="page__container">
-        <div className="content">
-          <h1 className="title">MAIN PAGE</h1>
-          <p className="description">
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Sapiente dolores ut earum saepe
-            illum voluptates, praesentium iusto asperiores corporis laborum harum officia ullam
-            excepturi nobis voluptatum exercitationem sunt. Dolor, quae!
-          </p>
-        </div>
-      </section>
-    </>
+    <section>
+      <div>main</div>
+    </section>
   );
 };
