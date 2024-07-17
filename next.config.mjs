2b8c60cc9a05ff5c651bf64ef427cf8da66c512d
Dany <danila.lusuy@gmail.com>
2024-07-17 18:23:40 +0300

    feat: add rewrites for change path

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,13 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  async rewrites() {
+    return [
+      {
+        source: "/exchanger-:exchanger",
+        destination: "/exchanger/:exchanger",
+      },
+    ];
+  },
+};
 
 export default nextConfig;
