74b38f7664ee2486c91da69090fdd20b28726c80
Dany <danila.lusuy@gmail.com>
2024-07-25 23:55:47 +0300

    fix: delete rewrites

--- next.config.mjs
+++ next.config.mjs
@@ -1,13 +1,4 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  async rewrites() {
-    return [
-      {
-        source: "/crypto-exchangers/exchanger/:exchanger",
-        destination: "/crypto-exchangers/exchanger-:exchanger",
-      },
-    ];
-  },
-};
+const nextConfig = {};
 
 export default nextConfig;
