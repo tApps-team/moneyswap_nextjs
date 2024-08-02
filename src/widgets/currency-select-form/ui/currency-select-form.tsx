c5e3db619acf11c9695883afb2404f4ee8101fff
Dany <danila.lusuy@gmail.com>
2024-08-02 13:24:09 +0300

    feat: add widget currency-select-form

--- src/widgets/currency-select-form/ui/currency-select-form.tsx
+++ src/widgets/currency-select-form/ui/currency-select-form.tsx
@@ -1,78 +1,141 @@
 "use client";
-import { zodResolver } from "@hookform/resolvers/zod";
-import { useEffect, useState } from "react";
-import { useForm } from "react-hook-form";
-import { z } from "zod";
+import { cx } from "class-variance-authority";
+import { useRouter, useSearchParams } from "next/navigation";
+import { useEffect } from "react";
 import { CurrecnySwitcher, CurrencySelect } from "@/features/currency";
-import { Currency, CurrencyResponse, getAvailableValutes } from "@/entities/currency";
+import { LocationSelect } from "@/features/location";
+import { useCurrecnyStore, useGetAvailableValutes } from "@/entities/currency";
+import { useDirectionStore } from "@/entities/direction";
+import { useGetCountries, useLocationStore } from "@/entities/location";
 import { cn } from "@/shared/lib";
 import { directions } from "@/shared/types";
-import { Button, Form } from "@/shared/ui";
-const formSchema = z.object({
-  valuteFrom: z.string().optional(),
-  valuteTo: z.string().optional(),
-  tab: z.string().optional(),
-});
-export const CurrecnySelectForm = () => {
-  const [tabValue, setTabValue] = useState<directions.cash | directions.noncash>(directions.cash);
+import { Button } from "@/shared/ui";
 
-  const [giveCurrencies, setGiveCurrencies] = useState<CurrencyResponse[]>();
-  const [getCurrencies, setGetCurrencies] = useState<CurrencyResponse[]>();
-  const form = useForm<z.infer<typeof formSchema>>({
-    resolver: zodResolver(formSchema),
-    defaultValues: {},
+type CurrecnySelectFormProps = {
+  url?: string;
+};
+export const CurrecnySelectForm = (props: CurrecnySelectFormProps) => {
+  const { url } = props;
+  const router = useRouter();
+
+  const searchParams = useSearchParams();
+  const location = useLocationStore((state) => state.location);
+  const {
+    getCurrency,
+    giveCurrency,
+    getCashCurrency,
+    giveCashCurrency,
+    setCashGetCurrency,
+    setCashGiveCurrency,
+    setGetCurrency,
+    setGiveCurrency,
+    resetCashCurrencies,
+    resetNoCashCurrencies,
+  } = useCurrecnyStore((state) => state);
+
+  const { direction, setDirection } = useDirectionStore((state) => state);
+
+  const currentGiveCurrency = direction === directions.cash ? giveCashCurrency : giveCurrency;
+  const currentGetCurrency = direction === directions.cash ? getCashCurrency : getCurrency;
+
+  const currentSetGiveCurrency =
+    direction === directions.cash ? setCashGiveCurrency : setGiveCurrency;
+  const currentSetGetCurrency = direction === directions.cash ? setCashGetCurrency : setGetCurrency;
+
+  const { data: countries } = useGetCountries();
+
+  const {
+    data: giveCurrencies,
+    isLoading: giveCurrenciesIsLoading,
+    isError: giveCurrenciesIsError,
+  } = useGetAvailableValutes({
+    base: "all",
+    city: direction === directions.cash ? location?.cityCodeName : undefined,
   });
-  useEffect(() => {
-    getAvailableValutes({ base: "all" }).then((data) => setGiveCurrencies(data));
-  }, []);
 
-  const onSubmit = (values: z.infer<typeof formSchema>) => {
-    console.log(values);
-  };
+  const {
+    data: getCurrencies,
+    isLoading: getCurrenciesIsLoading,
+    isError: getCurrenciesIsError,
+  } = useGetAvailableValutes({
+    base: currentGiveCurrency?.code_name,
+    city: direction === directions.cash ? location?.cityCodeName : undefined,
+  });
+
+  useEffect(() => {
+    if (getCurrenciesIsError) {
+      direction === directions.cash ? resetCashCurrencies() : resetNoCashCurrencies();
+    }
+  }, [getCurrenciesIsError, resetCashCurrencies, resetNoCashCurrencies, direction]);
+  useEffect(() => {
+    const valuteNotEmpty = currentGiveCurrency && currentGetCurrency;
+    if (valuteNotEmpty && direction !== directions.cash) {
+      router.push(`/exchange/${currentGiveCurrency.code_name}-to-${currentGetCurrency.code_name}`);
+    }
+    if (valuteNotEmpty && direction === directions.cash) {
+      router.push(
+        `/exchange/${currentGiveCurrency.code_name}-to-${currentGetCurrency.code_name}/${location?.cityCodeName}`,
+      );
+    }
+  }, [currentGetCurrency, currentGiveCurrency, location?.cityCodeName, router, direction]);
+
   return (
-    <Form {...form}>
-      <form
-        className="text-white h-72 py-4 px-7 bg-[#16192e] rounded-lg"
-        onSubmit={form.handleSubmit(onSubmit)}
-      >
-        <div className=" flex items-center justify-between">
-          <p>Выберите направление обмена</p>
-          <div className="flex gap-4 bg-[#2d3049] rounded-[4px] p-1">
-            <Button
-              type="button"
-              role="tab"
-              id="changeCash"
-              className={cn(
-                "bg-transparent rounded-[4px] py-2 px-6 hover:brightness-125 ",
-                tabValue === directions.cash && "bg-[#16192e]",
-              )}
-              onClick={() => setTabValue(directions.cash)}
-            >
-              Обмен наличных
-            </Button>
-            <Button
-              type="button"
-              role="tab"
-              id="changeOnline"
-              className={cn(
-                "bg-transparent rounded-[4px] py-2 px-6 hover:brightness-125",
-                tabValue === directions.noncash && "bg-[#16192e]",
-              )}
-              onClick={() => setTabValue(directions.noncash)}
-            >
-              Обмен онлайн
-            </Button>
-          </div>
+    // <Form {...form}>
+    <form className="text-white h-72 py-4 px-7 bg-[#16192e] rounded-lg">
+      <div className=" flex items-center justify-between">
+        <p>Выберите направление обмена</p>
+        <div className="flex gap-4 bg-[#2d3049] rounded-[4px] p-1">
+          <Button
+            type="button"
+            role="tab"
+            id="changeCash"
+            className={cn(
+              "bg-transparent rounded-[4px] py-2 px-6 hover:brightness-125 ",
+              direction === directions.cash && "bg-[#16192e]",
+            )}
+            onClick={() => setDirection(directions.cash)}
+          >
+            Обмен наличных
+          </Button>
+          <Button
+            type="button"
+            role="tab"
+            id="changeOnline"
+            className={cn(
+              "bg-transparent rounded-[4px] py-2 px-6 hover:brightness-125",
+              direction === directions.noncash && "bg-[#16192e]",
+            )}
+            onClick={() => setDirection(directions.noncash)}
+          >
+            Обмен онлайн
+          </Button>
         </div>
-        <div>
-          <div className="flex items-center justify-between">
-            <CurrencySelect currencies={giveCurrencies} label="Отдаете" />
-            <CurrecnySwitcher />
-            <CurrencySelect currencies={getCurrencies} label="Получаете" />
-          </div>
+      </div>
+      <div>
+        <div className={cx("flex items-center justify-between")}>
+          <CurrencySelect
+            onClick={currentSetGiveCurrency}
+            disabled={(direction === directions.cash && !location) || !giveCurrencies}
+            currencyInfo={currentGiveCurrency}
+            currencies={giveCurrencies}
+            label="Отдаете"
+          />
+          <CurrecnySwitcher />
+          <CurrencySelect
+            onClick={currentSetGetCurrency}
+            currencyInfo={currentGetCurrency}
+            disabled={
+              (!currentGetCurrency && !currentGiveCurrency) ||
+              (!currentGetCurrency && !getCurrencies)
+            }
+            currencies={getCurrencies}
+            label="Получаете"
+          />
+
+          {direction === directions.cash && <LocationSelect countries={countries || []} />}
         </div>
-        <Button>Submit</Button>
-      </form>
-    </Form>
+      </div>
+    </form>
+    // </Form>
   );
 };
