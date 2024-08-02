a6a1a1fadd911c5c86957a1682d9b20d9569f3e7
Dany <danila.lusuy@gmail.com>
2024-08-02 13:15:06 +0300

    fix: remove or delete

--- src/entities/currency/api/currency-dto.tsx
+++
@@ -1,7 +0,0 @@
-import { CurrencyResponse } from "../model/types/currencyType";
-
-export type GetAvailableValutesDtoResponse = CurrencyResponse[];
-export type GetAvailableValutesDtoRequest = {
-  city?: string;
-  base: string;
-};
