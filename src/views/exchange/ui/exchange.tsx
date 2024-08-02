4c988688a9d33802589ae7522198dab9e2e0e6f3
Dany <danila.lusuy@gmail.com>
2024-08-02 13:23:37 +0300

    feat: add ExchnagePage

--- src/views/exchange/ui/exchange.tsx
+++ src/views/exchange/ui/exchange.tsx
@@ -1,11 +1,34 @@
+import { usePathname } from "next/navigation";
+import { CurrecnySelectForm } from "@/widgets/currency-select-form";
+import { columns, ExchangersTable } from "@/widgets/exchangers";
+import { DataTableDemo } from "@/widgets/exchangers/exchangers-table/ui/table";
 import { FAQ } from "@/widgets/faq";
 import { SeoFooterText, SeoHeaderText } from "@/widgets/seo-text";
 
-export const ExchnagePage = ({ params }: { params: { slug: string[] } }) => {
+import { getSpecificValute } from "@/entities/currency";
+import { getExchangers } from "@/entities/exchanger";
+import { getSpecificCity } from "@/entities/location";
+
+export const ExchnagePage = async ({ params }: { params: { slug: string[] } }) => {
+  const slug = params.slug[0];
+  const city = params.slug[1];
+
+  const [valute_from, valute_to] = slug.split("-to-").map((str) => str.toLowerCase());
+  console.log(params);
+  const exchangers = await getExchangers({ valute_from, valute_to, city: city });
+  const giveCurrency = await getSpecificValute({ codeName: valute_from });
+  const getCurrency = await getSpecificValute({ codeName: valute_to });
+  const location = await getSpecificCity({ codeName: city });
+  console.log(giveCurrency);
+  console.log(getCurrency);
+  console.log(location);
+
   return (
     <div>
       <SeoHeaderText />
-      ExchnagePage
+      <CurrecnySelectForm />
+      <ExchangersTable columns={columns} data={exchangers} />
+
       <FAQ />
       <SeoFooterText />
     </div>
