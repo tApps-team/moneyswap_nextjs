01c140c7274caf65272966f97e6ceeac051bb2dc
Dany <danila.lusuy@gmail.com>
2024-08-07 12:10:29 +0300

    fix: comment code

--- src/widgets/main-faq/ui/main-faq.tsx
+++ src/widgets/main-faq/ui/main-faq.tsx
@@ -6,10 +6,10 @@ export const MainFAQ = async () => {
   // надо знать какое направление выбрано cash/noncash
   const direction = directions.cash;
 
-  const filteredFaqs = await getFaq(
-    direction === directions.cash ? mainFaqTypes.cash : mainFaqTypes.noncash,
-  );
-  const basicFaqs = await getFaq(mainFaqTypes.basic);
+  // const filteredFaqs = await getFaq(
+  //   direction === directions.cash ? mainFaqTypes.cash : mainFaqTypes.noncash,
+  // );
+  // const basicFaqs = await getFaq(mainFaqTypes.basic);
 
   return (
     <div className="grid grid-cols-2 py-[50px] gap-[5%] w-full">
@@ -21,13 +21,13 @@ export const MainFAQ = async () => {
               : "Вопросы по безналичному обмену"}
           </h3>
         </div>
-        <AccordionList data={filteredFaqs.data} />
+        {/* <AccordionList data={filteredFaqs.data} /> */}
       </div>
       <div className="w-full">
         <div className="pb-6 border-b-[#bbbbbb] border-b-[1.5px]">
           <h3 className="text-xl text-[#f6ff5f] font-[900] uppercase truncate">Общие вопросы</h3>
         </div>
-        <AccordionList data={basicFaqs.data} />
+        {/* <AccordionList data={basicFaqs.data} /> */}
       </div>
     </div>
   );
