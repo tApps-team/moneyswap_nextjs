db059d5b91bff014531a72cd47af9c49efcd68f6
Dania <danila.lusuy@gmail.com>
2024-08-08 18:53:28 +0300

    feat: add scroll area and some styles

--- src/features/location/loaction-select/ui/location-select.tsx
+++ src/features/location/loaction-select/ui/location-select.tsx
@@ -1,4 +1,4 @@
-import { ChevronDown, ChevronRight } from "lucide-react";
+import { ChevronDown, ChevronRight, SearchIcon } from "lucide-react";
 import Image from "next/image";
 import { useMemo, useState } from "react";
 import { useCurrecnyStore } from "@/entities/currency";
@@ -11,7 +11,15 @@ import {
   useLocationStore,
 } from "@/entities/location";
 import { useDebounce } from "@/shared/lib";
-import { Dialog, DialogClose, DialogContent, DialogTitle, DialogTrigger, Input } from "@/shared/ui";
+import {
+  Dialog,
+  DialogClose,
+  DialogContent,
+  DialogTitle,
+  DialogTrigger,
+  Input,
+  ScrollArea,
+} from "@/shared/ui";
 type LocationSelectProps = {
   countries: Country[];
 };
@@ -50,8 +58,8 @@ export const LocationSelect = (props: LocationSelectProps) => {
 
   return (
     <Dialog>
-      <DialogTrigger>
-        <div className="bg-[#2d3049] rounded-sm p-2 flex justify-between items-center gap-2">
+      <DialogTrigger asChild>
+        <div className="bg-[#2d2d2d] h-[60px] rounded-full border gap-2 border-[#bbbbbb] items-center p-2 flex ">
           {location && (
             <Image
               alt={`${location?.cityCodeName})`}
@@ -61,19 +69,29 @@ export const LocationSelect = (props: LocationSelectProps) => {
             />
           )}
 
-          <input readOnly value={location?.cityName} className="bg-transparent truncate  " />
+          <input
+            readOnly
+            value={location?.cityName}
+            className="bg-transparent truncate max-w-[100px]  "
+          />
           <div>
             <ChevronDown color="white" height={32} width={32} />
           </div>
         </div>
       </DialogTrigger>
-      <DialogContent className="min-w-[800px] text-black h-[600px]  ">
-        <DialogTitle className="m-0">Выбрать город</DialogTitle>
-        <Input
-          value={locationSearchValue}
-          onChange={(e) => setLocationSearchValue(e.target.value)}
-          placeholder="Поиск города и страны"
-        />
+      <DialogContent className="bg-[#2d2d2d] border-none w-[960px] h-[480px] shadow-black shadow-2xl rounded-2xl  grid  ">
+        <div className="grid grid-cols-2 grid-rows-1 items-center">
+          <DialogTitle className="m-0 uppercase">Выбор города</DialogTitle>
+          <div className="relative">
+            <SearchIcon className="absolute translate-y-2 left-3 " color="#bbbbbb" />
+            <Input
+              className="rounded-full bg-transparent pl-10 placeholder:uppercase placeholder:text-[#bbbbbb] placeholder:font-bold border-[#bbbbbb]"
+              value={locationSearchValue}
+              onChange={(e) => setLocationSearchValue(e.target.value)}
+              placeholder="Поиск города и страны"
+            />
+          </div>
+        </div>
         {debouncedLocationSearchValue ? (
           <div className="overflow-y-scroll flex flex-col gap-4 border rounded p-4">
             {filteredLocation.map((city) => (
@@ -92,35 +110,41 @@ export const LocationSelect = (props: LocationSelectProps) => {
           </div>
         ) : (
           <div className="grid grid-cols-[1fr,50px,1fr] grid-rows-1 min-h-full  ">
-            <div className="border rounded-sm  flex flex-col gap-3  overflow-y-scroll ">
-              {countries?.map((country) => (
-                <CountryCard
-                  active={country.id === location?.id}
-                  key={country.id}
-                  onClick={() => onClickCountry(country)}
-                  country={country}
-                />
-              ))}
-            </div>
-            <ChevronRight width={62} height={52} className="self-center" />
-            <div className="border rounded-sm  flex flex-col gap-3  overflow-y-scroll ">
-              {cityList?.cities.map((city) => (
-                <DialogClose key={city.id}>
-                  <CityCard
-                    onClick={() =>
-                      onClickCity({
-                        cityCodeName: city.code_name,
-                        countryIconUrl: cityList.icon_url,
-                        countryName: cityList.name.ru,
-                        cityName: city.name.ru,
-                        id: city.id,
-                      })
-                    }
-                    city={city}
+            <ScrollArea className="h-full border rounded-2xl p-4 ">
+              <div className="flex flex-col gap-3">
+                {[...countries, ...countries, ...countries]?.map((country) => (
+                  <CountryCard
+                    active={country.id === location?.id}
+                    key={country.id}
+                    onClick={() => onClickCountry(country)}
+                    country={country}
                   />
-                </DialogClose>
-              ))}
-            </div>
+                ))}
+              </div>
+            </ScrollArea>
+
+            <ChevronRight width={62} height={52} className="self-center" />
+            <ScrollArea className="h-full border rounded-2xl p-4">
+              <div className="  flex flex-col gap-3 ">
+                {cityList?.cities.map((city) => (
+                  <DialogClose key={city.id}>
+                    <CityCard
+                      onClick={() =>
+                        onClickCity({
+                          cityCodeName: city.code_name,
+                          countryIconUrl: cityList.icon_url,
+                          countryName: cityList.name.ru,
+                          cityName: city.name.ru,
+                          id: city.id,
+                        })
+                      }
+                      city={city}
+                      active={location?.cityCodeName === city.code_name}
+                    />
+                  </DialogClose>
+                ))}
+              </div>
+            </ScrollArea>
           </div>
         )}
       </DialogContent>
