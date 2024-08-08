db9b574bde7861bf651a0bc0472e622e9a2680b5
Dania <danila.lusuy@gmail.com>
2024-08-08 18:53:15 +0300

    feat: add scroll area and some styles

--- src/features/currency/currency-select/ui/currency-select.tsx
+++ src/features/currency/currency-select/ui/currency-select.tsx
@@ -1,5 +1,5 @@
 import { cx } from "class-variance-authority";
-import { Bitcoin, ChevronDown } from "lucide-react";
+import { Bitcoin, ChevronDown, SearchIcon } from "lucide-react";
 import Image from "next/image";
 import { useRouter } from "next/navigation";
 import { useDeferredValue, useEffect, useState } from "react";
@@ -13,6 +13,7 @@ import {
   DialogTitle,
   DialogTrigger,
   Input,
+  ScrollArea,
   Tabs,
   TabsContent,
   TabsList,
@@ -55,20 +56,20 @@ export const CurrencySelect = (props: CurrencySelectProps) => {
     .filter((tab) => tab?.currencies?.length > 0);
 
   return (
-    <div className={cx("flex flex-col ", disabled && "pointer-events-none ")}>
+    <div className={cx("flex flex-col  ", disabled && "pointer-events-none ")}>
       {label && <p className="uppercase">{label}</p>}
       <div className="flex items-center border border-[#bbbbbb]  rounded-full bg-gradient-to-l from-[#606060] gap-10 justify-between  ">
         <input
           disabled={disabled}
           type="number"
-          className="bg-transparent text-[#f6ff5f] px-6 "
+          className="bg-transparent text-[#f6ff5f] px-6 max-w-[100px] ml-4"
           value={amount || ""}
           onChange={(e) => setAmount(e.target.valueAsNumber)}
         />
         <Dialog>
-          <DialogTrigger disabled={disabled}>
-            <div className="bg-[#2d2d2d]  rounded-full items-center p-2 flex h-full">
-              <div className="flex gap-2">
+          <DialogTrigger disabled={disabled} asChild>
+            <div className="bg-[#2d2d2d] min-h-14 rounded-full items-center p-2 flex h-full">
+              <div className="flex gap-2 ">
                 {currencyInfo ? (
                   <Image
                     alt={`${currencyInfo?.name?.ru} (${currencyInfo?.code_name})`}
@@ -79,52 +80,58 @@ export const CurrencySelect = (props: CurrencySelectProps) => {
                 ) : (
                   <Bitcoin />
                 )}
-                {/* <div className="flex flex-col items-start">
-                <div>{currencyInfo?.name?.ru}</div>
-                <div>{currencyInfo?.code_name}</div>
-              </div> */}
+
                 <input
                   readOnly
-                  className="bg-transparent "
+                  className="bg-transparent max-w-[150px] truncate"
                   value={currencyInfo ? currencyInfo?.name?.ru : "Наличные руб"}
                 />
               </div>
-              <ChevronDown />
+              <ChevronDown width={28} height={28} />
             </div>
           </DialogTrigger>
-          <DialogContent className="min-w-[800px] h-[600px]  ">
-            <DialogTitle className="m-0">Выбор валюты</DialogTitle>
-            <Input
-              value={searchValue}
-              onChange={(e) => setSearchValue(e.target.value)}
-              placeholder="Поиск валюты"
-            />
-            <Tabs defaultValue={"Все"} className="h-[400px]   overflow-y-scroll">
-              <TabsList>
-                {filteredTabList.map((tab) => (
-                  <TabsTrigger key={tab.id} value={tab.name.ru}>
-                    {tab.name.ru}
-                  </TabsTrigger>
-                ))}
-              </TabsList>
-              {filteredTabList.map((tab) => (
-                <TabsContent
-                  className="grid mt-0 grid-rows-1 gap-2"
-                  value={tab.name.ru}
-                  key={tab.id}
-                >
-                  {tab.currencies.map((currency) => (
-                    <DialogClose key={currency.id}>
-                      <CurrencyCard
-                        onClick={() => onClick(currency)}
-                        key={currency.id}
-                        currency={currency}
-                      />
-                    </DialogClose>
+          <DialogContent className="bg-[#2d2d2d] border-none  shadow-black shadow-2xl rounded-2xl  grid  ">
+            <div className="grid grid-cols-2 items-center">
+              <DialogTitle className=" uppercase text-xl">Выбор валюты</DialogTitle>
+              <div className="relative">
+                <SearchIcon className="absolute translate-y-2 left-3 " color="#bbbbbb" />
+                <Input
+                  className="rounded-full bg-transparent pl-10 placeholder:uppercase placeholder:text-[#bbbbbb] placeholder:font-bold border-[#bbbbbb]"
+                  value={searchValue}
+                  onChange={(e) => setSearchValue(e.target.value)}
+                  placeholder="Поиск валюты"
+                />
+              </div>
+            </div>
+            <ScrollArea className="h-full ">
+              <Tabs defaultValue={"Все"} className="min-h-[480px] max-h-[480px] min-w-[500px]">
+                <TabsList className=" bg-[#2d2d2d] h-10  grid grid-flow-col gap-6 justify-start">
+                  {filteredTabList.map((tab) => (
+                    <TabsTrigger
+                      className="data-[state=active]:bg-[#f6ff5f] uppercase rounded-full text-sm border-[#bbbbbb] border"
+                      key={tab.id}
+                      value={tab.name.ru}
+                    >
+                      {tab.name.ru}
+                    </TabsTrigger>
                   ))}
-                </TabsContent>
-              ))}
-            </Tabs>
+                </TabsList>
+
+                {filteredTabList.map((tab) => (
+                  <TabsContent className="grid grid-rows-1 gap-2" value={tab.name.ru} key={tab.id}>
+                    {tab.currencies.map((currency) => (
+                      <DialogClose key={currency.id}>
+                        <CurrencyCard
+                          onClick={() => onClick(currency)}
+                          key={currency.id}
+                          currency={currency}
+                        />
+                      </DialogClose>
+                    ))}
+                  </TabsContent>
+                ))}
+              </Tabs>
+            </ScrollArea>
           </DialogContent>
         </Dialog>
       </div>
