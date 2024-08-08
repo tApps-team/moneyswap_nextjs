c7c2127b4d1e2fe7e437ef31cdcc3f6ca3b58651
Dania <danila.lusuy@gmail.com>
2024-08-08 18:55:10 +0300

    feat: add fetch give and get currecnies for main select form

--- src/views/main/ui/main.tsx
+++ src/views/main/ui/main.tsx
@@ -1,11 +1,14 @@
 import { CurrecnySelectForm } from "@/widgets/currency-select-form";
 import { MainFAQ } from "@/widgets/main-faq";
 import { SeoFooterText, SeoHeaderText } from "@/widgets/seo-text";
+import { getSpecificValute } from "@/entities/currency";
 import { getSeoTexts } from "@/shared/api";
-import { pageTypes } from "@/shared/types";
+import { directions, pageTypes } from "@/shared/types";
 
 export const Main = async () => {
   // const seoTexts = await getSeoTexts({ page: pageTypes.main });
+  const giveCurrency = await getSpecificValute({ codeName: "BTC" });
+  const getCurrency = await getSpecificValute({ codeName: "SBERRUB" });
   return (
     <section>
       {/* <SeoHeaderText data={seoTexts.data} /> */}
