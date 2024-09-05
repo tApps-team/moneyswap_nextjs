cc32c530011ce82e664e041e3138717218b87769
Dania <danila.lusuy@gmail.com>
2024-09-05 22:31:15 +0300

    feat: add public api

--- src/shared/ui/index.ts
+++ src/shared/ui/index.ts
@@ -1,21 +1,10 @@
-export { Input } from "./input";
-export { Tabs, TabsContent, TabsList, TabsTrigger } from "./tabs";
-export { Card, CardContent, CardDescription, CardFooter, CardHeader, CardTitle } from "./card";
-export { Separator } from "./separator";
-export { Accordion, AccordionContent, AccordionItem, AccordionTrigger } from "./accordion";
-export { Button, buttonVariants } from "./button";
-export {
-  Dialog,
-  DialogClose,
-  DialogContent,
-  DialogDescription,
-  DialogFooter,
-  DialogHeader,
-  DialogOverlay,
-  DialogPortal,
-  DialogTitle,
-  DialogTrigger,
-} from "./dialog";
+export * from "./input";
+export * from "./tabs";
+export * from "./card";
+export * from "./separator";
+export * from "./accordion";
+export * from "./button";
+export * from "./dialog";
 export * from "./form";
 export * from "./label";
 export * from "./table";
@@ -23,3 +12,4 @@ export * from "./dropdown-menu";
 export * from "./checkbox";
 export * from "./scroll-area";
 export * from "./carousel";
+export * from "./hover-card";
