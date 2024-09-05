793704dea98003277fceed81ddf1d2f69190e544
Dania <danila.lusuy@gmail.com>
2024-09-05 22:37:38 +0300

    feat: create CryptoTable for render exchnager data

---
+++ src/widgets/crypto-exchangers/crypto-table/ui/crypto-table.tsx
@@ -0,0 +1,118 @@
+"use client";
+
+import {
+  ColumnDef,
+  ColumnFiltersState,
+  flexRender,
+  getCoreRowModel,
+  getFilteredRowModel,
+  getPaginationRowModel,
+  getSortedRowModel,
+  SortingState,
+  useReactTable,
+  VisibilityState,
+} from "@tanstack/react-table";
+import Link from "next/link";
+import { useState } from "react";
+import { Exchanger } from "@/entities/exchanger";
+import { Button, Table, TableBody, TableCell, TableHead, TableHeader, TableRow } from "@/shared/ui";
+import { cryptoColumns } from "../model/columns";
+import { CryptoTableColumns } from "../model/columns";
+import { mockData } from "../model/mockData";
+
+interface DataTableProps<TValue> {
+  //   columns: ColumnDef<CryptoTableColumns, TValue>[];
+  //   data: CryptoTableColumns[];
+}
+
+export function CryptoTable<TData, TValue>() {
+  const [sorting, setSorting] = useState<SortingState>([]);
+  const [columnFilters, setColumnFilters] = useState<ColumnFiltersState>([]);
+  const [columnVisibility, setColumnVisibility] = useState<VisibilityState>({});
+  const [rowSelection, setRowSelection] = useState({});
+  const [pagination, setPagination] = useState({
+    pageIndex: 0,
+    pageSize: 10,
+  });
+
+  const table = useReactTable({
+    data: mockData,
+    columns: cryptoColumns,
+    onSortingChange: setSorting,
+    getCoreRowModel: getCoreRowModel(),
+    onColumnFiltersChange: setColumnFilters,
+    manualFiltering: false,
+    getPaginationRowModel: getPaginationRowModel(),
+    getSortedRowModel: getSortedRowModel(),
+    getFilteredRowModel: getFilteredRowModel(),
+    onColumnVisibilityChange: setColumnVisibility,
+    onRowSelectionChange: setRowSelection,
+    state: {
+      sorting,
+      columnFilters,
+      columnVisibility,
+      rowSelection,
+      pagination,
+    },
+  });
+
+  const handleShowMore = () => {
+    setPagination((prev) => ({
+      ...prev,
+      pageSize: prev.pageSize + 10,
+    }));
+  };
+
+  return (
+    <div className="flex flex-col mt-10 gap-12 w-full">
+      <div className="rounded-3xl bg-[#2d2d2d] text-white shadow-[1px_3px_10px_3px_rgba(0,0,0,0.7)]">
+        <Table className="">
+          <TableHeader className="">
+            {table?.getHeaderGroups()?.map((headerGroup) => (
+              <TableRow key={headerGroup.id}>
+                {headerGroup.headers.map((header) => (
+                  <TableHead
+                    className="uppercase py-6 px-4 text-[#bbbbbb] font-semibold"
+                    key={header.id}
+                  >
+                    {header.isPlaceholder
+                      ? null
+                      : flexRender(header.column.columnDef.header, header.getContext())}
+                  </TableHead>
+                ))}
+              </TableRow>
+            ))}
+          </TableHeader>
+          <TableBody>
+            {table.getRowModel().rows?.length ? (
+              table.getRowModel().rows.map((row) => (
+                <TableRow key={row.id} data-state={row.getIsSelected() && "selected"}>
+                  {row.getVisibleCells().map((cell) => (
+                    <TableCell key={cell.id}>
+                      {flexRender(cell.column.columnDef.cell, cell.getContext())}
+                    </TableCell>
+                  ))}
+                </TableRow>
+              ))
+            ) : (
+              <TableRow>
+                <TableCell colSpan={cryptoColumns.length} className="h-24 text-center">
+                  No results.
+                </TableCell>
+              </TableRow>
+            )}
+          </TableBody>
+        </Table>
+      </div>
+      <Button
+        onClick={handleShowMore}
+        disabled={
+          table.getRowModel().rows.length >= mockData.length || table.getRowModel().rows.length < 1
+        }
+        className="bg-[#2d2d2d] h-14 w-[200px] mx-auto border-2 border-[#bbbbbb] uppercase rounded-full"
+      >
+        Показать ещё
+      </Button>
+    </div>
+  );
+}
