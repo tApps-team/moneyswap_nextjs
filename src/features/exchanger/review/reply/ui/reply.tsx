040ef197ddf599e1194dbcc62a7a0442f6423cb7
Dania <danila.lusuy@gmail.com>
2024-09-10 12:07:12 +0300

    feat: create Reply button

---
+++ src/features/exchanger/review/reply/ui/reply.tsx
@@ -0,0 +1,12 @@
+import { Button } from "@/shared/ui";
+
+export const Reply = () => {
+  return (
+    <Button
+      variant={"ghost"}
+      className="bg-[#f6ff5f] uppercase rounded-full text-black px-6 font-semibold"
+    >
+      Ответить
+    </Button>
+  );
+};
