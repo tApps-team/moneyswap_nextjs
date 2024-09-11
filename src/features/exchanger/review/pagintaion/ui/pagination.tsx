346d8dea91bb679e01532931fd4fedc9a0394b25
Dania <danila.lusuy@gmail.com>
2024-09-11 12:42:39 +0300

    feat: create ExchangerPagination

--- src/features/exchanger/review/pagintaion/ui/pagination.tsx
+++ src/features/exchanger/review/pagintaion/ui/pagination.tsx
@@ -1,3 +1,36 @@
+import {
+  Pagination,
+  PaginationContent,
+  PaginationEllipsis,
+  PaginationItem,
+  PaginationLink,
+  PaginationNext,
+  PaginationPrevious,
+} from "@/shared/ui";
+
 export const ExchangerPagination = () => {
-  return <div></div>;
+  return (
+    <Pagination>
+      <PaginationPrevious href="#" />
+      <PaginationContent className="rounded-full border">
+        <PaginationItem></PaginationItem>
+        <PaginationItem className="">
+          <PaginationLink className="" href="#">
+            1
+          </PaginationLink>
+        </PaginationItem>
+        <PaginationItem>
+          <PaginationLink href="#">2</PaginationLink>
+        </PaginationItem>
+        <PaginationItem>
+          <PaginationLink href="#">3</PaginationLink>
+        </PaginationItem>
+        <PaginationItem>
+          <PaginationEllipsis />
+        </PaginationItem>
+        <PaginationItem></PaginationItem>
+      </PaginationContent>
+      <PaginationNext href="#" />
+    </Pagination>
+  );
 };
