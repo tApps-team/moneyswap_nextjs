735a59ebdba50e47c30f73336e55009737c5b4d5
Dania <danila.lusuy@gmail.com>
2024-09-12 23:17:20 +0300

    feat: create ReviweFilter

--- src/features/exchanger/review/review-filter/ui/review-filter.tsx
+++ src/features/exchanger/review/review-filter/ui/review-filter.tsx
@@ -11,14 +11,16 @@ type ReviweFilterProps = {
 export const ReviweFilter = (props: ReviweFilterProps) => {
   const { reviewGrade } = props;
   const searchParams = useSearchParams();
-  const createUrl = () => {};
+  const createUrl = () => {
+    const url = `crypto-exchangers/exchnager`;
+  };
   return (
     <div className="grid grid-cols-4 gap-6">
       {filterData.map((data) => (
         <Link
           key={data.href}
           className="border hover:border-[#f6ff5f] hover:bg-[#f6ff5f] hover:text-black  text-center rounded-full bg-[#2d2d2d] p-4 text-xs font-medium"
-          href={""}
+          href={"/"}
         >
           <p>{data.value}</p>
         </Link>
