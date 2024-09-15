3c6f668430d005a713a7dc3e9e8148c7f5e8f065
Dania <danila.lusuy@gmail.com>
2024-09-16 00:37:51 +0300

    feat: refacotr query, remove url

--- src/entities/exchanger-review/api/exchanger-review-api.ts
+++ src/entities/exchanger-review/api/exchanger-review-api.ts
@@ -7,11 +7,10 @@ import {
 export const reviewsByExchange = async (
   props: ReviewsByExchangeDTORequest,
 ): Promise<ReviewsByExchangeDTOResponse> => {
-  const { exchange_id, exchange_marker, grade_filter, page, element_on_page } = props;
-  const url = `api/reviews/reviews_by_exchange?exchange_id=${exchange_id}&exchange_marker=${exchange_marker}&page=${page}&element_on_page=3&grade_filter=${element_on_page}`;
   const response = await apiClient.get<ReviewsByExchangeDTOResponse>(
     "api/reviews/reviews_by_exchange",
     props,
   );
+
   return response;
 };
