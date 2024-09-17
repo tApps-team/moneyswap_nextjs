87331e764eee2ca8a8866f3098a98e040e18e0b3
SenezAr <danila.lusuy@gmail.com>
2024-09-17 16:34:31 +0300

    feat: add pagination logic

--- src/features/exchanger/review/pagintaion/ui/pagination.tsx
+++ src/features/exchanger/review/pagintaion/ui/pagination.tsx
@@ -13,16 +13,19 @@ import {
 type ExchangerPaginationProps = {
   totalPages: number;
 };
+// Todo  handle if total page = 0
 export const ExchangerPagination = (props: ExchangerPaginationProps) => {
   const { totalPages } = props;
   const pathname = usePathname();
   const searchParams = useSearchParams();
   const currentPage = Number(searchParams.get("page")) || 1;
+
   const createPageURL = (pageNumber: number | string) => {
     const params = new URLSearchParams(searchParams);
     params.set("page", pageNumber.toString());
     return `${pathname}?${params.toString()}`;
   };
+
   const createPrevPage = () => {
     const params = new URLSearchParams(searchParams);
     if (currentPage > 0) {
@@ -30,6 +33,7 @@ export const ExchangerPagination = (props: ExchangerPaginationProps) => {
     }
     return `${pathname}?${params.toString()}`;
   };
+
   const createNextPage = () => {
     const params = new URLSearchParams(searchParams);
     if (currentPage < totalPages) {
@@ -37,28 +41,21 @@ export const ExchangerPagination = (props: ExchangerPaginationProps) => {
     }
     return `${pathname}?${params.toString()}`;
   };
+  const paginationArray = Array.from(Array(totalPages), (item, index) => index + 1);
+  console.log(paginationArray);
   return (
     <Pagination>
-      <PaginationPrevious href={createPrevPage()} />
+      <PaginationPrevious scroll={false} href={createPrevPage()} />
       <PaginationContent className="rounded-full border">
-        <PaginationItem></PaginationItem>
-        <PaginationItem className="">
-          <PaginationLink scroll={false} className="" href="#">
-            1
-          </PaginationLink>
-        </PaginationItem>
-        <PaginationItem>
-          <PaginationLink href="#">2</PaginationLink>
-        </PaginationItem>
-        <PaginationItem>
-          <PaginationLink href="#">3</PaginationLink>
-        </PaginationItem>
-        <PaginationItem>
-          <PaginationEllipsis />
-        </PaginationItem>
-        <PaginationItem></PaginationItem>
+        {paginationArray.map((paginationItem) => (
+          <PaginationItem key={paginationItem}>
+            <PaginationLink className="" scroll={false} href={createPageURL(paginationItem)}>
+              {paginationItem}
+            </PaginationLink>
+          </PaginationItem>
+        ))}
       </PaginationContent>
-      <PaginationNext href={createNextPage()} />
+      <PaginationNext scroll={false} href={createNextPage()} />
     </Pagination>
   );
 };
