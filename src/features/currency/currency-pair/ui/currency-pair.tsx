3d8b5a51d0dfd2a995781043496b9f79f7b1e580
SenezAr <danila.lusuy@gmail.com>
2024-09-23 18:26:09 +0300

    feat: refactor code, add createUrl fn

--- src/features/currency/currency-pair/ui/currency-pair.tsx
+++ src/features/currency/currency-pair/ui/currency-pair.tsx
@@ -3,16 +3,21 @@ import Link from "next/link";
 import { CurrencyPair as CurrenyPairType, SpecificValute } from "@/entities/currency";
 import { PopularArrowIcon } from "@/shared/assets";
 import { routes } from "@/shared/router";
+import { ExchangerMarker } from "@/shared/types";
 
 type CurrencyPairProps = {
   currencyPair: CurrenyPairType;
 };
 export const CurrencyPair = (props: CurrencyPairProps) => {
   const { currencyPair } = props;
-
+  const createUrl = () => {
+    if (currencyPair.direction_type === ExchangerMarker.cash)
+      return `${routes.home}exchange/${currencyPair.valuteFrom.code_name}-to-${currencyPair.valuteTo.code_name}?direction=${currencyPair.direction_type}`;
+    return `${routes.home}exchange/${currencyPair.valuteFrom.code_name}-to-${currencyPair.valuteTo.code_name}`;
+  };
   return (
     <Link
-      href={`${routes.home}exchange/${currencyPair.valuteFrom.code_name}-to-${currencyPair.valuteTo.code_name}`}
+      href={createUrl()}
       className="px-2 py-2 grid grid-flow-col grid-rows-1 justify-between items-center rounded-[35px] shadow-[5px_5px_10px_0px_rgba(0,0,0,0.7)] bg-[#2d2d2d] hover:shadow-[5px_5px_5px_0px_rgba(0,0,0,0.7)] hover:scale-[1.01] transition-all duration-300"
     >
       <Image
