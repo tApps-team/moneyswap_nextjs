314392579177dd83548a504be5234f2907738b2f
SenezAr <danila.lusuy@gmail.com>
2024-09-23 18:25:38 +0300

    feat: create LocationInfo type

--- src/entities/location/country/model/country-types.ts
+++ src/entities/location/country/model/country-types.ts
@@ -7,3 +7,12 @@ export type Country = {
   icon_url: string;
   cities: City[];
 };
+export type LocationInfo = {
+  id: number;
+  name: Name;
+  code_name: string;
+  country: {
+    name: Name;
+    icon_url: string;
+  };
+};
