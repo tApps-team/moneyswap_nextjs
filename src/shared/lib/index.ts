7ba0db1244a659a0170f7d6bf1b4dc4c323b1164
SenezAr <danila.lusuy@gmail.com>
2024-09-24 18:07:06 +0300

    feat: add public api

--- src/shared/lib/index.ts
+++ src/shared/lib/index.ts
@@ -2,3 +2,4 @@ export { cn } from "./utils";
 export { useThrottle } from "./hooks/useThrottle";
 export { useDebounce } from "./hooks/useDebounce";
 export * from "./hooks/useLocalStorage";
+export * from "./is-empty-object/is-empty-object";
