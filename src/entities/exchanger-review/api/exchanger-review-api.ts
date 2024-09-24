532f0cb108fb89e05deb6a750f4ecfeb44fcf931
SenezAr <danila.lusuy@gmail.com>
2024-09-24 18:05:09 +0300

    feat: add getCommentsByReview endpoint

--- src/entities/exchanger-review/api/exchanger-review-api.ts
+++ src/entities/exchanger-review/api/exchanger-review-api.ts
@@ -1,5 +1,7 @@
 import { apiClient } from "@/shared/api";
 import {
+  GetCommentsByReviewDtoRequest,
+  GetCommentsByReviewDtoResponse,
   ReviewsByExchangeDTORequest,
   ReviewsByExchangeDTOResponse,
 } from "./exchanger-review-api-dto";
@@ -13,5 +15,13 @@ export const reviewsByExchange = async (
     "no-store",
   );
 
+  return response;
+};
+
+export const getCommentsByReview = async (props: GetCommentsByReviewDtoRequest) => {
+  const { exchangerId, exchangerMarker, reviewId } = props;
+  const response = await apiClient.get<GetCommentsByReviewDtoResponse>(
+    `api/reviews/get_comments_by_review?exchange_id=${exchangerId}&exchange_marker=${exchangerMarker}&review_id=${reviewId}`,
+  );
   return response;
 };
