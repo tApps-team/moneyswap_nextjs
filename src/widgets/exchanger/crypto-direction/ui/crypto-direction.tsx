ed0fc6ff8a06f592766cdaeda3f2c740a744d341
SenezAr <danila.lusuy@gmail.com>
2024-09-24 18:08:11 +0300

    feat: add sorting currency pair

--- src/widgets/exchanger/crypto-direction/ui/crypto-direction.tsx
+++ src/widgets/exchanger/crypto-direction/ui/crypto-direction.tsx
@@ -2,12 +2,14 @@ import { CurrencyPair } from "@/features/currency/currency-pair";
 import { CurrencyPair as CurrencyPairType } from "@/entities/currency";
 import { cn } from "@/shared/lib";
 import { ScrollArea } from "@/shared/ui";
+import { sortingCurrencyPair } from "../lib/sorting-currency-pair";
 
 type CryptoDirectionProps = {
   currencyPair: CurrencyPairType[];
 };
 export const CryptoDirection = async (props: CryptoDirectionProps) => {
   const { currencyPair } = props;
+  const sortedCurrencyPair = sortingCurrencyPair(currencyPair);
   const maxPointsCount = Math.max(...currencyPair.map((item) => item.pairCount));
   return (
     <aside className="grid grid-cols-1 grid-flow-row min-h-[36rem] max-h-[40rem] overflow-hidden  gap-4 items-start rounded-2xl  bg-[#2d2d2d] shadow-[1px_3px_10px_3px_rgba(0,0,0,0.7)] p-5">
@@ -18,7 +20,7 @@ export const CryptoDirection = async (props: CryptoDirectionProps) => {
 
       <ScrollArea className="h-full  pr-3">
         <div className="flex flex-col gap-4  p-1">
-          {currencyPair.map((pair, index) => (
+          {sortedCurrencyPair.map((pair, index) => (
             <div
               className="grid grid-cols-[0.7fr,1fr]   gap-6 w-full "
               // key={pair.valute_from.name.en + pair.pointsCount}
