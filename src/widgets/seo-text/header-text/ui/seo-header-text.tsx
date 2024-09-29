e01c43a348d4734d468f659de0b6d83d74bcf09a
Dania <danila.lusuy@gmail.com>
2024-09-30 01:05:59 +0300

    feat: add Hover card

--- src/widgets/seo-text/header-text/ui/seo-header-text.tsx
+++ src/widgets/seo-text/header-text/ui/seo-header-text.tsx
@@ -1,6 +1,7 @@
 import { FC } from "react";
 import { YellowQuestionIcon, YoutubeIcon } from "@/shared/assets";
 import { SeoTextsBlock } from "@/shared/types";
+import { HoverCard, HoverCardContent, HoverCardTrigger } from "@/shared/ui";
 
 export const SeoHeaderText: FC<SeoTextsBlock> = ({ data }) => {
   return (
@@ -13,7 +14,26 @@ export const SeoHeaderText: FC<SeoTextsBlock> = ({ data }) => {
               dangerouslySetInnerHTML={{ __html: data[0]?.header_title }}
             />
             <div className="absolute top-0 right-0 grid grid-flow-col gap-4 justify-center items-center">
-              <YellowQuestionIcon width={36} height={36} className="cursor-pointer" />
+              <HoverCard>
+                <HoverCardTrigger asChild>
+                  <YellowQuestionIcon width={36} height={36} className="cursor-pointer" />
+                </HoverCardTrigger>
+                <HoverCardContent className="rounded-2xl border-none shadow-[1px_3px_10px_3px_rgba(0,0,0,0.7)] min-w-[500px] p-8 flex flex-col gap-4 text-sm font-medium text-white bg-[#2d2d2d]">
+                  <p>
+                    1. ВЫБЕРИТЕ ВАЛЮТУ, КОТОРУЮ ХОТИТЕ ОТДАТЬ И ВАЛЮТУ, КОТОРУЮ ХОТИТЕ ПОЛУЧИТЬ.
+                  </p>
+                  <p>2. ВЫБЕРИТЕ ГОРОД, В КОТОРОМ ХОТИТЕ СОВЕРШИТЬ ОБМЕН.</p>
+                  <p>3. ВЫБЕРИТЕ ОБМЕННИК ИЗ СПИСКА - У ПЕРВОГО ОБМЕННИКА ЛУЧШИЙ КУРС.</p>
+                  <p>4. ПЕРЕЙДИТЕ НА САЙТ ОБМЕННИКА И СОЗДАЙТЕ ЗАЯВКУ НА САЙТЕ.</p>
+                  <p>5. С ВАМИ СВЯЖЕТСЯ МЕНЕДЖЕР.</p>
+                  <p>
+                    6. ДОГОВОРИТЕСЬ ПО ВРЕМЕНИ И ПРИЕЗЖАЙТЕ В ОФИС ИЛИ ЗАКАЖИТЕ ДОСТАВКУ НАЛИЧНЫХ.
+                  </p>
+                  <p>
+                    7. ПОДЕЛИТЕСЬ СВОИМ ОПЫТОМ С ДРУГИМИ ПОЛЬЗОВАТЕЛЯМИ, ОСТАВИВ ОТЗЫВ НА MONEYSWAP.
+                  </p>
+                </HoverCardContent>
+              </HoverCard>
               {/* <YoutubeIcon width={36} height={36} /> */}
               <img src="/youtube.svg" alt="" width={37} height={37} className="cursor-pointer" />
             </div>
