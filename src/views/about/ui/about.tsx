b81a0eef436d17289817a562a7d3f1ed6101a846
Dania <danila.lusuy@gmail.com>
2024-10-02 23:07:43 +0300

    feat: add coins and exchnager widgets

--- src/views/about/ui/about.tsx
+++ src/views/about/ui/about.tsx
@@ -1,7 +1,18 @@
+import { TopCoins } from "@/widgets/about/top-coins";
+import { TopExchangers } from "@/widgets/about/top-exchngers";
 import { ArticleContent } from "@/widgets/strapi";
+
 import { getAboutPage } from "@/entities/strapi";
 
 export const AboutPage = async () => {
   const { data } = await getAboutPage();
-  return <ArticleContent dynamic_content={data?.content} />;
+  return (
+    <section className="grid grid-cols-[0.7fr,0.3fr]">
+      <ArticleContent dynamic_content={data?.content} />
+      <aside className="flex flex-col gap-6">
+        <TopCoins />
+        <TopExchangers />
+      </aside>
+    </section>
+  );
 };
