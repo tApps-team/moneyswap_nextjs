64e727ec5ec12cb1a0680a9ab6e35b89b6b75381
Dania <danila.lusuy@gmail.com>
2024-10-02 23:08:03 +0300

    feat: add feedbackform widget

--- src/views/for-partners/ui/for_partners.tsx
+++ src/views/for-partners/ui/for_partners.tsx
@@ -1,7 +1,13 @@
+import { FeedbackForm } from "@/widgets/feedback-form";
 import { ArticleContent } from "@/widgets/strapi";
 import { getForPartnersPage } from "@/entities/strapi";
 
 export const ForPartnersPage = async () => {
   const { data } = await getForPartnersPage();
-  return <ArticleContent dynamic_content={data.content} />;
+  return (
+    <section className="grid grid-cols-[0.7fr,0.3fr]">
+      <ArticleContent dynamic_content={data.content} />
+      <FeedbackForm />
+    </section>
+  );
 };
