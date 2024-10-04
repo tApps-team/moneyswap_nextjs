856c619d7dc32be996b04ef884948e0081c8f50b
Dania <danila.lusuy@gmail.com>
2024-10-04 09:53:26 +0300

    feat: add getTopExchanger query

--- src/views/about/ui/about.tsx
+++ src/views/about/ui/about.tsx
@@ -1,10 +1,11 @@
 import { TopCoins } from "@/widgets/about/top-coins";
-import { TopExchangers } from "@/widgets/about/top-exchngers";
+import { TopExchangers, getTopExchangers } from "@/widgets/about/top-exchngers";
 import { ArticleContent } from "@/widgets/strapi";
 
 import { getAboutPage } from "@/entities/strapi";
 
 export const AboutPage = async () => {
+  const topExchangers = getTopExchangers();
   const { data } = await getAboutPage();
   return (
     <section className="grid grid-cols-[0.7fr,0.3fr]">
