50379b804a4c818ea3c3a0b1b817a0863c20705d
Dania <danila.lusuy@gmail.com>
2024-10-11 00:31:45 +0300

    fix: remove classname

--- src/shared/assets/icons/switcher.tsx
+++ src/shared/assets/icons/switcher.tsx
@@ -8,11 +8,11 @@ export const SwitcherIcon = (props: any) => (
   >
     <path
       d="m14.87 5.64-.01.01v-.02H0v5.03h9.63L0 19.88l3.72 3.55L18.59 9.2l-3.72-3.56z"
-      className="cls-1"
+      // className="cls-1"
     />
     <path
       d="m27.55 12.77 9.62-9.21L33.46 0l-9.61 9.2V0H18.6v9.21h5.24l-5.25 5.03h-5.25v9.2h5.26v-9.2l3.71 3.55.01-.01v.02h14.86v-5.03h-9.63z"
-      className="cls-1"
+      // className="cls-1"
     />
   </svg>
 );
