9b6b2e4d8c5a6e21b1473407f35c04b6c2f7b1c1
Dania <danila.lusuy@gmail.com>
2024-10-11 00:29:30 +0300

    feat: add key id and code name

--- src/widgets/about/top-coins/ui/top-coins.tsx
+++ src/widgets/about/top-coins/ui/top-coins.tsx
@@ -4,77 +4,11 @@ import { cn } from "@/shared/lib";
 import { ScrollArea } from "@/shared/ui";
 import { getTopCoins } from "..";
 
-// type TopCoins = {
-//   id: number;
-//   iconUrl: string;
-//   name: string;
-//   code_name: string;
-//   course: number;
-//   isIncrease: boolean;
-//   percent: number;
-// };
-// const topCoins: TopCoins[] = [
-//   {
-//     id: 1,
-//     course: 23232323,
-//     name: "Bitcoin",
-//     isIncrease: true,
-//     percent: 22,
-//     code_name: "BTC",
-//     iconUrl: "https://api.moneyswap.online/media/icons/valute/BTC.svg",
-//   },
-//   {
-//     id: 2,
-//     course: 1000,
-
-//     isIncrease: false,
-//     percent: 22,
-//     name: "Ethereum",
-//     code_name: "ETH",
-//     iconUrl: "https://api.moneyswap.online/media/icons/valute/ETH.svg",
-//   },
-//   {
-//     id: 3,
-//     course: 2323,
-//     name: "Tether TRC20 USDT",
-//     isIncrease: true,
-//     percent: 22,
-//     code_name: "USDTTRC20",
-//     iconUrl: "https://api.moneyswap.online/media/icons/valute/USDTTRC20.svg",
-//   },
-//   {
-//     id: 4,
-//     course: 123,
-
-//     isIncrease: true,
-//     percent: 22,
-//     name: "Litecoin",
-//     code_name: "LTC",
-//     iconUrl: "https://api.moneyswap.online/media/icons/valute/LTC.svg",
-//   },
-//   {
-//     id: 5,
-//     course: 123,
-
-//     isIncrease: true,
-//     percent: 22,
-//     name: "Litecoin",
-//     code_name: "LTC",
-//     iconUrl: "https://api.moneyswap.online/media/icons/valute/LTC.svg",
-//   },
-//   {
-//     id: 6,
-//     course: 123,
-
-//     isIncrease: true,
-//     percent: 22,
-//     name: "Litecoin",
-//     code_name: "LTC",
-//     iconUrl: "https://api.moneyswap.online/media/icons/valute/LTC.svg",
-//   },
-// ];
 export const TopCoins = async () => {
+  // await new Promise((resolve) => setTimeout(resolve, 3000));
+
   const topCoins = await getTopCoins();
+  console.log(topCoins);
 
   return (
     <div className="bg-[#2d2d2d]   grid grid-cols-1 grid-flow-row gap-4 shadow-[1px_3px_10px_3px_rgba(0,0,0,0.7)] p-6 rounded-2xl">
@@ -83,7 +17,7 @@ export const TopCoins = async () => {
         <div className="flex flex-col gap-4 ">
           {topCoins?.map((topCoin) => (
             <div
-              key={topCoin.id}
+              key={topCoin.id + topCoin.code_name}
               className="flex pb-3 border-b last:border-none justify-between items-center"
             >
               <div className="flex gap-2 items-center">
