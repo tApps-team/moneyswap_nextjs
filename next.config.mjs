a69a957b1aac31ab17581fefacc3a9ed480ba75d
Dania <danila.lusuy@gmail.com>
2024-10-11 00:46:19 +0300

    feat: add remotePatterns

--- next.config.mjs
+++ next.config.mjs
@@ -1,5 +1,16 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "api.moneyswap.online",
+        port: "",
+        pathname: "/media/icons/valute/**",
+      },
+    ],
+    formats: ["image/webp"],
+  },
   async rewrites() {
     return [
       {
