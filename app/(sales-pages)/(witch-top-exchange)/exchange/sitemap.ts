162e6561c3c048f64c552d00a3f1583b4c09130e
Dania <danila.lusuy@gmail.com>
2024-10-28 23:42:39 +0300

    feat: created sitemap

---
+++ app/(sales-pages)/(witch-top-exchange)/exchange/sitemap.ts
@@ -0,0 +1,14 @@
+import { MetadataRoute } from "next";
+import { getSitemapDirections } from "@/entities/exchanger";
+import { baseUrl } from "@/shared/consts";
+import { routes } from "@/shared/router";
+export const revalidate = 3600;
+export default async function sitemap(): Promise<MetadataRoute.Sitemap> {
+  const directions = await getSitemapDirections();
+  return directions.map((direction) => ({
+    url: `${baseUrl}${routes.exchange.replace("/", "")}/${direction.valute_from}-to-${direction.valute_to}${direction.city ? `?city=${direction.city}` : ""}`,
+    priority: 0.7,
+    lastModified: new Date(),
+    changeFrequency: "daily",
+  }));
+}
