6a74207f346aeb71e58e701f08bc5cac7b91227e
AbddSsh <abduraimovshakh@gmail.com>
2024-11-15 10:44:09 +0500

    feat: main faq adaptive

--- src/features/strapi/faq/accordion/ui/accordion.tsx
+++ src/features/strapi/faq/accordion/ui/accordion.tsx
@@ -1,4 +1,5 @@
 import parse, { DOMNode, Element } from "html-react-parser";
+import { ChevronDown } from "lucide-react";
 import Image from "next/image";
 import React, { FC } from "react";
 import { MainFaqs } from "@/entities/strapi";
@@ -19,18 +20,19 @@ const options = {
 
 export const AccordionList: FC<MainFaqs> = ({ data }) => {
   return (
-    <Accordion type="single" collapsible className="w-full grid gap-6">
+    <Accordion type="single" collapsible className="w-full grid mobile-xl:gap-6 gap-6">
       {data?.map((faq) => (
         <AccordionItem
           value={`Value-${faq?.id}`}
           key={faq?.id}
-          className="grid grid-flow-row mobile-xl:rounded-[25px] rounded-xl shadow-[1px_3px_5px_3px_rgba(0,0,0,0.3)] bg-dark-gray px-8 py-4"
+          className="grid grid-flow-row mobile-xl:rounded-[25px] rounded-none mobile-xl:shadow-[1px_3px_5px_3px_rgba(0,0,0,0.3)] shadow-none mobile-xl:bg-dark-gray bg-transparent mobile-xl:px-8 mobile-xl:py-4 p-0 pb-6 mobile-xl:border-none border-b-2 border-light-gray"
         >
-          <AccordionTrigger className="text-sm font-medium tracking-wider text-start uppercase color-[#fff] p-0 border-b-0 [&>svg]:-mr-[20px] [&[data-state=open]]:text-white [&[data-state=open]>svg]:stroke-yellow-main hover:text-white leading-4">
+          <AccordionTrigger className="mobile-xl:text-sm text-xs font-semibold tracking-wider text-start uppercase color-[#fff] p-0 border-b-0 mobile-xl:[&>svg]:-mr-5 [&[data-state=open]]:text-white [&[data-state=open]>svg]:stroke-yellow-main hover:text-white mobile-xl:leading-6 leading-5 [&>svg]:w-6 [&>svg]:h-6 [&>svg]:stroke-[3px]">
             {faq?.question}
+            <ChevronDown className="transition-all duration-300 shrink-0" />
           </AccordionTrigger>
           <AccordionContent className="pb-0">
-            <div className="mt-6 text-xs font-normal uppercase text-white strapi_styles strapi_fonts_codec">
+            <div className="mobile-xl:mt-6 mt-6 font-normal uppercase text-white strapi_styles strapi_fonts_codec">
               {parse(faq?.answer, options)}
             </div>
           </AccordionContent>
