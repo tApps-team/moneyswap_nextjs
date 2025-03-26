351d83e7e67d7c4354802c029d82c14125fd3e65
AbddSsh <abduraimovshakh@gmail.com>
2025-03-26 18:47:49 +0500

    fix: bg color

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,3 +1,121 @@
+// // eslint-disable-next-line import/order
+// import { Viewport } from "next";
+// import Script from "next/script";
+// import Head from "next/head";
+// import Providers from "@/app/providers/react-query";
+// import "@/shared/styles/globals.scss";
+// import { Footer } from "@/widgets/footer";
+// import { Header } from "@/widgets/header";
+// import NextTopLoader from 'nextjs-toploader';
+
+// export const metadata = {
+//   title: "Мониторинг криптообменников онлайн - обмен криптовалюты по лучшим курсам | MoneySwap",
+//   description:
+//     "MoneySwap - удобный помощник для поиска обменников в любой точке мира. На нашей площадке представлены только проверенные обменники с безупречной репутацией. Незаменимый помощник в мире финансов.",
+//   metadataBase: new URL(process.env.NEXT_PUBLIC_SITE_BASE_URL || ""),
+//   openGraph: {
+//     title: "Мониторинг криптообменников онлайн - обмен криптовалюты по лучшим курсам | MoneySwap",
+//     description:
+//       "MoneySwap - удобный помощник для поиска обменников в любой точке мира. На нашей площадке представлены только проверенные обменники с безупречной репутацией. Незаменимый помощник в мире финансов.",
+//     url: process.env.NEXT_PUBLIC_SITE_BASE_URL,
+//     siteName: "MoneySwap",
+//     images: [
+//       {
+//         url: "/og_logo.svg",
+//         width: 400,
+//         height: 283,
+//         alt: "MoneySwap",
+//       },
+//     ],
+//     locale: "ru-RU",
+//     type: "website",
+//   },
+//   icons: {
+//     icon: [
+//       { url: '/favicon.ico', sizes: 'any' },
+//       { url: '/favicon/favicon-16x16.png', sizes: '16x16', type: 'image/png' },
+//       { url: '/favicon/favicon-32x32.png', sizes: '32x32', type: 'image/png' },
+//     ],
+//     apple: [
+//       { url: '/favicon/apple-touch-icon.png' },
+//     ],
+//     shortcut: [{ url: '/favicon.ico' }],
+//     other: [
+//       {
+//         rel: 'mask-icon',
+//         url: '/favicon/safari-pinned-tab.svg',
+//         color: '#000000'
+//       },
+//     ],
+//   },
+//   manifest: '/favicon/site.webmanifest',
+// };
+// export const viewport: Viewport = {
+//   initialScale: 1,
+//   width: "device-width",
+// };
+// export default function RootLayout({ children }: { children: React.ReactNode }) {
+//   return (
+//     <html lang="ru">
+//       <head>
+//         <meta
+//           name="google-site-verification"
+//           content="sPwXL6jSpXeyCa3nGMpepemssgj5Fjd4ZRpE9lofLfc"
+//         />
+//         <meta name="yandex-verification" content="7377d71acbace068" />
+//         <meta name="msapplication-TileColor" content="#ffffff" />
+//         <meta name="theme-color" content="#ffffff" />
+//         {/* Google Tag Manager */}
+//         <Script id="gtm-script" strategy="afterInteractive">
+//           {`
+//             (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+//             new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+//             j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+//             'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+//             })(window,document,'script','dataLayer','GTM-582D3SVQ');
+//           `}
+//         </Script>
+//         {/* Google Tag Manager */}
+//       </head>
+//       <Head>
+//         <link rel="icon" href="/public/favicon/favicon.ico" />
+//         <link rel="icon" href="/favicon/favicon.ico" />
+//       </Head>
+//       <body className="flex flex-col min-h-screen">
+//         <NextTopLoader 
+//           color="#F6FF5F"
+//           initialPosition={0.08}
+//           crawlSpeed={200}
+//           height={4}
+//           crawl={true}
+//           showSpinner={false}
+//           easing="ease"
+//           speed={200}
+//         />
+//         {/* Google Tag Manager (noscript) */}
+//         <noscript>
+//           <iframe
+//             title="Google Tag Manager"
+//             src="https://www.googletagmanager.com/ns.html?id=GTM-582D3SVQ"
+//             height="0"
+//             width="0"
+//             style={{ display: "none", visibility: "hidden" }}
+//           />
+//         </noscript>
+//         {/* Google Tag Manager (noscript) */}
+//         <Providers>
+//           <Header />
+//           <main className="flex-grow max-w-[1400px] mx-[auto] w-full py-[40px] lg:pt-[130px] pt-[90px] mobile-xl:px-[25px] px-[15px]">
+//             {children}
+//           </main>
+//           <Footer />
+//         </Providers>
+//       </body>
+//     </html>
+//   );
+// }
+
+
 // eslint-disable-next-line import/order
 import { Viewport } from "next";
 import Script from "next/script";
@@ -31,24 +149,8 @@ export const metadata = {
     type: "website",
   },
   icons: {
-    icon: [
-      { url: '/favicon.ico', sizes: 'any' },
-      { url: '/favicon/favicon-16x16.png', sizes: '16x16', type: 'image/png' },
-      { url: '/favicon/favicon-32x32.png', sizes: '32x32', type: 'image/png' },
-    ],
-    apple: [
-      { url: '/favicon/apple-touch-icon.png' },
-    ],
-    shortcut: [{ url: '/favicon.ico' }],
-    other: [
-      {
-        rel: 'mask-icon',
-        url: '/favicon/safari-pinned-tab.svg',
-        color: '#000000'
-      },
-    ],
+    icon: "/favicon/favicon.ico",
   },
-  manifest: '/favicon/site.webmanifest',
 };
 export const viewport: Viewport = {
   initialScale: 1,
@@ -63,8 +165,6 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
           content="sPwXL6jSpXeyCa3nGMpepemssgj5Fjd4ZRpE9lofLfc"
         />
         <meta name="yandex-verification" content="7377d71acbace068" />
-        <meta name="msapplication-TileColor" content="#ffffff" />
-        <meta name="theme-color" content="#ffffff" />
         {/* Google Tag Manager */}
         <Script id="gtm-script" strategy="afterInteractive">
           {`
@@ -80,6 +180,7 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
       <Head>
         <link rel="icon" href="/public/favicon/favicon.ico" />
         <link rel="icon" href="/favicon/favicon.ico" />
+        <link rel="icon" href="/favicon.ico" />
       </Head>
       <body className="flex flex-col min-h-screen">
         <NextTopLoader 
