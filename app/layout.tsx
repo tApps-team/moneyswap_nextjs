b380fa4cd864965abcc3b4db5d90b3e95eb63f2d
AbddSsh <abduraimovshakh@gmail.com>
2025-04-04 11:24:23 +0500

    fix: favicon.ico files

--- app/layout.tsx
+++ app/layout.tsx
@@ -34,8 +34,7 @@ export const metadata = {
   icons: {
     icon: [
       { url: '/favicon.ico', sizes: 'any' },
-      { url: '/favicon/favicon-16x16.png', sizes: '16x16', type: 'image/png' },
-      { url: '/favicon/favicon-32x32.png', sizes: '32x32', type: 'image/png' },
+      { url: '/favicon/favicon-96x96.png', sizes: '16x16', type: 'image/png' },
     ],
     apple: [
       { url: '/favicon/apple-touch-icon.png' },
